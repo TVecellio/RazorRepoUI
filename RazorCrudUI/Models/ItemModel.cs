503d261a16f48ba1a97ff7c4155e143ae5463e32
Thomas Vecellio <tjv00008@mix.wvu.edu>
2024-02-19 12:49:25 -0500

    Created the new project

---
+++ RazorCrudUI/Models/ItemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorCrudUI.Models;
+    public class ItemModel
+    {
+    public int Id { get; set; }
+
+    public String Name { get; set; }
+
+    public String Description { get; set; }
+
+    public decimal Price { get; set; }
+}
+
