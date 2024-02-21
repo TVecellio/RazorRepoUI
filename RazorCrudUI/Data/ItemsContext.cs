b301ead72f819bc61764aa213a2d98a72a0768d9
Thomas Vecellio <tjv00008@mix.wvu.edu>
2024-02-21 12:36:25 -0500

    Connected to the database

---
+++ RazorCrudUI/Data/ItemsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorCrudUI.Models;
+
+namespace RazorCrudUI.Data
+{
+    public class ItemsContext : DbContext
+    { 
+        public ItemsContext(DbContextOptions<ItemsContext> options) : base(options)
+        { 
+        
+        }
+                        // Items is the table name
+        public DbSet<ItemModel> Items { get; set; } = default;
+    }
+}
--- RazorCrudUI/Program.cs
+++ RazorCrudUI/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using RazorCrudUI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddDbContext<ItemsContext>(options =>
+        options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default")));
+        //database is connected
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
