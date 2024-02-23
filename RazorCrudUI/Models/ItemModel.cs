55642a4deb7277ecc24dbf143ca2c758135e3574
Thomas Vecellio <tjv00008@mix.wvu.edu>
2024-02-23 12:38:51 -0500

    implementing the scaffolding in the database

--- RazorCrudUI/Models/ItemModel.cs
+++ RazorCrudUI/Models/ItemModel.cs
@@ -1,8 +1,11 @@
-﻿namespace RazorCrudUI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorCrudUI.Models;
     public class ItemModel
     {
     public int Id { get; set; }
 
+    [Display(Name = "Item Name")]
     public String Name { get; set; }
 
     public String Description { get; set; }
---
+++ RazorCrudUI/Pages/Items/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorCrudUI.Data;
+using RazorCrudUI.Models;
+
+namespace RazorCrudUI.Pages.Items
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorCrudUI.Data.ItemsContext _context;
+
+        public IndexModel(RazorCrudUI.Data.ItemsContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ItemModel> ItemModel { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            ItemModel = await _context.Items.ToListAsync();
+        }
+    }
+}
